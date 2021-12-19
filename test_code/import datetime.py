f5af05a5312b5662f0681a445b97bbc1b75a6e91
anijya <kanika003500@gmail.com>
2021-12-19 15:48:47 +0900

    first commit

---
+++ test_code/import datetime.py
@@ -0,0 +1,18 @@
+import datetime
+import os
+
+kamyu = "カミュ"
+print(kamyu[0])
+print(kamyu[1])
+print(kamyu[2])
+
+dokode = "どこで？ だれが？ いつ？".split(" ")
+print(dokode)
+
+list = ["The", "fox", "jumped", "over", "the", "fence", "."]
+print(" ".join(list).replace("e", "$"))
+print("Hemingway".index("m"))
+
+word = "四月の晴れた寒い日で、時計がどれも十三時を打っていた。"
+print(word[:10])
+
